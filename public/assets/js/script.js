2a98269b41ab3c1881b8a21cc8a4d684f770e696
momen162 <miah15-4789@diu.edu.bd>
2025-07-08 22:19:02 +0600

    script js file updated(2)

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,5 +3,5 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("EventEase ready");
 });
 
-//Here we will script.js code for hamburger click
+//Here we will add script.js code for hamburger click
 
