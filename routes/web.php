4f702cc02980116357549ab4b28d092b8765ae46
MasudBinMazid <mamun15-5451@diu.edu.bd>
2025-07-09 00:17:03 +0600

    route updated

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/events', function () {
+    return view('events');
+});
+Route::get('/gallery', function () {
+    return view('gallery');
+});
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/login', function () {
+    return view('login'); // optional
+});
+
+
+
+
+
+
+
