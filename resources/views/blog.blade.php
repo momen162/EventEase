dc36d0452b0d1a4df87daa1dd678ac4f843b2d7d
tanizinnat <tani15-5315@diu.edu.bd>
2025-07-10 23:39:23 +0600

    blog structure build

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -7,10 +7,28 @@
 @endsection
 
 @section('content')
-  <h2>Latest Blogs</h2>
-  <div class="blog-list">
-    <!-- Blog articles -->
-  </div>
+  <section class="blog-container">
+    <h2>Latest Blogs</h2>
+    <div class="blog-list">
+      <div class="blog-card">
+        <img src="{{ asset('assets/images/blog1.jpg') }}" alt="Blog Image">
+        <div class="blog-content">
+          <h3>Understanding Laravel Blade</h3>
+          <p class="blog-snippet">Laravel Blade makes templating easier and faster. Learn how to master it for dynamic views.</p>
+          <button class="read-more-btn">Read More</button>
+        </div>
+      </div>
+
+      <div class="blog-card">
+        <img src="{{ asset('assets/images/blog2.jpg') }}" alt="Blog Image">
+        <div class="blog-content">
+          <h3>JavaScript ES6 Tricks</h3>
+          <p class="blog-snippet">Explore new ES6 features like arrow functions, template literals, destructuring, and more.</p>
+          <button class="read-more-btn">Read More</button>
+        </div>
+      </div>
+    </div>
+  </section>
 @endsection
 
 @section('extra-js')
