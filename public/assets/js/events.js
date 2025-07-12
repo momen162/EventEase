ad489b348672119fa61071f1e60b8ef692430893
momen162 <miah15-4789@diu.edu.bd>
2025-07-13 01:12:26 +0600

    js for making search filtering functional

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -22,7 +22,35 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+//javascript for search filtering
+document.addEventListener('DOMContentLoaded', () => {
+  const filterRadios = document.querySelectorAll('input[name="eventStatus"]');
+  const cards = document.querySelectorAll('.event-card');
+  const searchBtn = document.getElementById('triggerSearch');
+  const searchInput = document.getElementById('searchInput');
 
+  function filterEvents() {
+    const selectedStatus = document.querySelector('input[name="eventStatus"]:checked').value;
+    const keyword = searchInput.value.toLowerCase();
+
+    cards.forEach(card => {
+      const status = card.getAttribute('data-status');
+      const textContent = card.textContent.toLowerCase();
+
+      const matchStatus = (selectedStatus === 'all' || selectedStatus === status);
+      const matchKeyword = textContent.includes(keyword);
+
+      card.style.display = (matchStatus && matchKeyword) ? 'block' : 'none';
+    });
+  }
+
+  filterRadios.forEach(radio => {
+    radio.addEventListener('change', filterEvents);
+  });
+
+  searchBtn.addEventListener('click', filterEvents);
+
+});
 
 
 
