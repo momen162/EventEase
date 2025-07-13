c9a7cfcfa1ea9317fc6c62c866af9020acf8d840
MasudBinMazid <mamun15-5451@diu.edu.bd>
2025-07-13 15:12:16 +0600

    routes for social and profile controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -32,35 +31,30 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
 
 require __DIR__.'/auth.php';
 
 
 
 
+// Auth Routes
+
 use App\Http\Controllers\Auth\SocialController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/auth/google', [SocialController::class, 'redirectToGoogle']);
 Route::get('/auth/google/callback', [SocialController::class, 'handleGoogleCallback']);
+
 Route::get('/auth/facebook', [SocialController::class, 'redirectToFacebook']);
 Route::get('/auth/facebook/callback', [SocialController::class, 'handleFacebookCallback']);
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('auth.dashboard');
-    })->name('dashboard');
-
-    Route::get('/profile/edit', function () {
-        return view('auth.edit-profile');
-    })->name('profile.edit');
-    
-    Route::post('/profile/update', [App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/dashboard', [ProfileController::class, 'dashboard'])->name('dashboard');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
 });
+
 
 
 
@@ -99,3 +93,8 @@ Route::get('/gallery/event-{id}', function ($id) {
 
     return view('gallery-details', $events[$id]);
 });
+
+
+
+
+
