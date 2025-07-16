a3e08e32b4385cca1110dbe77795bc951cb1053a
tanizinnat <tani15-5315@diu.edu.bd>
2025-07-16 15:25:03 +0600

    blog view html updated

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -7,14 +7,17 @@
 @endsection
 
 @section('content')
-<div class="blog-details">
-  <div class="banner">
+<div class="blog-details modern-blog-container">
+  <div class="blog-banner">
     <img src="{{ asset('assets/images/' . $blog->image) }}" alt="Blog Banner">
   </div>
+
   <div class="blog-full-content">
-    <h1>{{ $blog->title }}</h1>
-    <p class="author">Author: {{ $blog->author }}</p>
-    <p>{{ $blog->full_content }}</p>
+    <h1 class="blog-title">{{ $blog->title }}</h1>
+    <p class="blog-author">✍️ Author: <strong>{{ $blog->author }}</strong></p>
+    <div class="blog-content">
+      {!! nl2br(e($blog->full_content)) !!}
+    </div>
   </div>
 </div>
 @endsection
