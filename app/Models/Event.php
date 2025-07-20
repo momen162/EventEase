b56b03e8b8455742388076368db7783378443c78
momen162 <miah15-4789@diu.edu.bd>
2025-07-20 12:10:00 +0600

    Add Event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,32 +6,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-protected $fillable = [
-  'title','description','location','venue','starts_at','ends_at','capacity',
-  'price','purchase_option','banner','created_by','status','approved_by','approved_at',
-];
-
-
-    protected $casts = [
-        'starts_at'   => 'datetime',
-        'ends_at'     => 'datetime',
-        'approved_at' => 'datetime',
-        'price'       => 'decimal:2',
-    ];
-
-    public function creator()
-    {
-        return $this->belongsTo(User::class, 'created_by');
-    }
-
-    public function approver()
-    {
-        return $this->belongsTo(User::class, 'approved_by');
-    }
-
-    // Optional: always get a usable URL for banner
-    public function getBannerUrlAttribute(): ?string
-    {
-        return $this->banner ? asset(ltrim($this->banner, '/')) : null;
-    }
 }
