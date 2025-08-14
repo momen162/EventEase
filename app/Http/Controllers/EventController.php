d6421ba36964932cd3c8f28d2a8c814ff3deaced
momen162 <miah15-4789@diu.edu.bd>
2025-08-14 15:00:00 +0600

    controller for event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,35 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
-use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
     public function index()
     {
-    }
-
-    public function create()
-    {
-    }
-
-    public function store(Request $request)
-    {
+        $events = Event::latest('starts_at')->paginate(9);
+        return view('events', ['events' => $events]);
     }
 
     public function show(Event $event)
     {
-    }
-
-    public function edit(Event $event)
-    {
-    }
-
-    public function update(Request $request, Event $event)
-    {
-    }
-
-    public function destroy(Event $event)
-    {
+        return view('event-details', ['event' => $event]);
     }
 }
