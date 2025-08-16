601e8daf7bf251fcd8451f862c3d4843be730c93
tanizinnat <tani15-5315@diu.edu.bd>
2025-08-16 15:04:22 +0600

    some chnages

--- public/admin/_layout_bottom.php
+++ public/admin/_layout_bottom.php
@@ -3,7 +3,7 @@
 <footer class="app-footer">
   <div class="wrap" style="text-align:center; padding:20px 0; color:#666; font-size:14px">
     <p>
-      © <?= date('Y') ?> EventEase Admin • 
+      © <?= date('Y') ?> EventEase • 
       Made by <a href="https://github.com/MasudBinMazid" target="_blank" rel="noopener noreferrer" style="color:#2563eb; text-decoration:none">Masud</a>
     </p>
   </div>
