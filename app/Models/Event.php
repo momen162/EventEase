2900bf2600ffe693eeb5ae43628ca557c016ad1a
momen162 <miah15-4789@diu.edu.bd>
2025-08-21 05:16:03 +0600

    event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,4 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    protected $fillable = [
+        'title', 'description', 'location',
+        'starts_at', 'ends_at', 'capacity', 'price'
+    ];
+
+    protected $dates = ['starts_at', 'ends_at'];
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
